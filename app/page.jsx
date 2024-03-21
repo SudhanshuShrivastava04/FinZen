d4ce974b334ff46d10a246553c1b22dcdbcb56ba
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-03-21 16:22:49 +0530

    Navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,14 @@
-
 export default function Home() {
   return (
-    <>
-      <p>hello</p>
-    </>
+    <main>
+      <section>
+        <small className="text-gray-400 text-md">My Balance</small>
+        <h2 className="text-4xl font-bold">$10000</h2>
+      </section>
+
+      <section>
+
+      </section>
+    </main>
   );
 }
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,10 +1,59 @@
+"use client";
+
+import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { IoIosStats } from "react-icons/io";
+import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
-export default function Navbar() {
+export function SignOutButton() {
+  return <Button className="bg-red-400 hover:bg-red-500">Sign Out</Button>;
+}
+
+export function MobileNav() {
+  const [open, setOpen] = useState(false);
   return (
-    <header className="w-full flex justify-between items-center">
+    <>
+      <Sheet>
+        <SheetTrigger className="sm:hidden flex justify-end w-full">
+          <GiHamburgerMenu size={20} className="text-white" />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle className="flex w-full items-center gap-4">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div>Hi, Sudhanshu</div>
+            </SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+          <SheetFooter>
+            <SignOutButton />
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
+
+export function DesktopNav() {
+  return (
+    <header className="w-full justify-between items-center mt-2 mb-6 relative hidden sm:flex">
       <div className="flex gap-2 items-center">
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
@@ -14,8 +63,17 @@ export default function Navbar() {
       </div>
       <nav className="flex items-center gap-2">
         <IoIosStats size={20} />
-        <Button className="bg-red-400 hover:bg-red-500">Sign Out</Button>
+        <SignOutButton />
       </nav>
     </header>
+  );
+}
+
+export default function Navbar() {
+  return (
+    <div className="w-full">
+      <MobileNav />
+      <DesktopNav />
+    </div>
   );
 }
